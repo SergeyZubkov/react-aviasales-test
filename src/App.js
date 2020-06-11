9a4a596d0288c2b8375ceeca2e91443bcf76fe95
Sergey Zubkov <svzubkovru@gmail.com>
2020-06-12 02:24:34 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <div className="transfer-filter box">
+          Количество пересадок
+        </div>
+      </div>
+      <div className="main">
+        <div className="sorting-pannel box">
+          <button>Самый дешевый</button>
+          <button>Самый быстрый</button>
+        </div>
+      </div>
     </div>
   );
 }
